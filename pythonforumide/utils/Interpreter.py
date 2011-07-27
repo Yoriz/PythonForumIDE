d761d2a018b52773c497bfaba36413da8e702b22
Jakob Bowyer <jkbbwr@gmail.com>
2011-07-27 21:50:18 +0100

    Starting the changes on interpreter.

--- pythonforumide/utils/Interpreter.py
+++ pythonforumide/utils/Interpreter.py
@@ -4,31 +4,13 @@ Created on Mon Jul 25 17:05:42 2011
 
 @author: jakob
 """
-try:
-    from cStringIO import StringIO
-except ImportError:
-    from StringIO import StringIO
-import subprocess
-import version
+from twisted.internet.protocol import ProcessProtocol
+from twisted.internet import reactor
 
-class Interpreter(object):
-    def __init__(self, filename):
-        self.python_exe = version.get_python_exe()
-    
-    def launch(self):    
-        self.python = subprocess.Popen(self.python_exe, 
-                                       stdout = subprocess.PIPE,
-                                        stderr = subprocess.PIPE,
-                                        stdin = subprocess.PIPE) 
-    def write(self, data):
-        print data
-    
-class IPythonInterpreter(Interpreter):
-    """In the future we might be embedding IPython"""
-    pass
-
-class BPythonInterpreter(Interpreter):
-    """In the future we might be embedding BPython"""
-    pass
-
-i = Interpreter()
+class PythonProcessProtocol(ProcessProtocol):
+    def __init__(self, text):
+        self.text = text
+        
+    def connectionMade(self):
+        self.transport.write(self.text)
+        self.transport.closeStdin()
